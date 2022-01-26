f6c24321184b02ea10095ce30c6a48773bf5359f
Hygon <hygon806@gmail.com>
2022-01-26 16:51:52 +0100

    Ameliorated the UI of the party form

--- app/src/main/java/fr/amgone/animaquizz/app/gui/PartyForm.java
+++ app/src/main/java/fr/amgone/animaquizz/app/gui/PartyForm.java
@@ -9,6 +9,7 @@ import fr.amgone.animaquizz.shared.items.Item;
 import fr.amgone.animaquizz.shared.items.TextItem;
 import fr.amgone.animaquizz.shared.packets.AnswerPacket;
 import org.jdesktop.swingx.JXTextField;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -20,49 +21,51 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.image.BufferedImage;
 
 public class PartyForm extends JPanel {
     private final Party party;
     private final Box playersBox;
     private final Box itemBox;
 
+    private BufferedImage image;
+    private int lastResizeWidth = 0;
+    private int lastResizeHeight = 0;
+
     public PartyForm(Client client, Party party) {
         this.party = party;
         this.setLayout(new BorderLayout());
         this.setBackground(new Color(49, 116, 158));
 
-        Box partyBox = Box.createVerticalBox();
         JLabel partyName = new JLabel(party.getName());
-        partyName.setPreferredSize(new Dimension(200, 80));
-        partyName.setMaximumSize(new Dimension(200, 80));
         partyName.setFont(new Font(Font.SERIF, Font.BOLD, 32));
         partyName.setHorizontalAlignment(JLabel.CENTER);
         partyName.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         partyName.setForeground(Color.BLACK);
-        partyBox.add(partyName);
+
+        partyName.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        this.add(partyName, BorderLayout.PAGE_START);
 
         itemBox = Box.createVerticalBox();
-        itemBox.setPreferredSize(new Dimension(800, 570));
-        itemBox.setMaximumSize(new Dimension(800, 570));
-        itemBox.setMinimumSize(new Dimension(800, 570));
         itemBox.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         itemBox.setBackground(Color.GRAY);
         itemBox.setOpaque(true);
 
         JLabel guess = new JLabel("Devine ça");
-        guess.setHorizontalAlignment(JLabel.CENTER);
-        guess.setVerticalAlignment(JLabel.CENTER);
         itemBox.add(guess);
-        partyBox.add(itemBox);
+        this.add(itemBox, BorderLayout.CENTER);
 
         ATextField answer = new ATextField(20);
         answer.setPrompt("Écrivez votre réponse");
         answer.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 24));
 
-        answer.setPreferredSize(new Dimension(900, 50));
-        answer.setMaximumSize(new Dimension(900, 50));
         answer.setAlignmentX(JXTextField.CENTER_ALIGNMENT);
         answer.setHorizontalAlignment(JXTextField.CENTER);
+        answer.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
         answer.addActionListener(actionEvent -> {
             if(!answer.getText().equals("")) {
@@ -70,13 +73,10 @@ public class PartyForm extends JPanel {
                 answer.setText("");
             }
         });
-        partyBox.add(answer);
 
-        this.add(partyBox, BorderLayout.CENTER);
+        this.add(answer, BorderLayout.PAGE_END);
 
         playersBox = Box.createVerticalBox();
-        playersBox.setMaximumSize(new Dimension(300, 600));
-        playersBox.setPreferredSize(new Dimension(300, 600));
         playersBox.setBackground(new Color(20, 25, 35));
         playersBox.setOpaque(true);
         this.add(playersBox, BorderLayout.LINE_END);
@@ -118,9 +118,59 @@ public class PartyForm extends JPanel {
         if(item instanceof TextItem textItem) {
             itemBox.add(new JLabel(textItem.getText()));
         } else if(item instanceof ImageItem imageItem) {
-            itemBox.add(new JLabel(new ImageIcon(imageItem.getBufferedImage())));
+            image = imageItem.getBufferedImage();
+            JLabel jLabel = new JLabel(new ImageIcon(resize(image, itemBox.getWidth(), itemBox.getHeight())));
+            itemBox.addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    if (lastResizeWidth != itemBox.getWidth() || lastResizeHeight != itemBox.getHeight()) {
+                        lastResizeWidth = itemBox.getWidth();
+                        lastResizeHeight = itemBox.getHeight();
+
+                        Dimension imageDimension = getScaledDimension(image.getWidth(), image.getHeight(), new Dimension(itemBox.getWidth(), itemBox.getHeight()));
+                        jLabel.setIcon(new ImageIcon(resize(image, (int) imageDimension.getWidth(), (int) imageDimension.getHeight())));
+                        updateUI();
+                        System.out.println("refreshed " + imageDimension.getWidth() + " " + imageDimension.getHeight());
+                    }
+                }
+            });
+            itemBox.add(jLabel);
         }
 
         reloadPlayers();
+    }
+
+    public static BufferedImage resize(BufferedImage image, int width, int height) {
+        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
+        Graphics2D g2d = bi.createGraphics();
+        g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
+        g2d.drawImage(image, 0, 0, width, height, null);
+        g2d.dispose();
+        return bi;
+    }
+
+    public static Dimension getScaledDimension(int original_width, int original_height, Dimension boundary) {
+        int bound_width = boundary.width;
+        int bound_height = boundary.height;
+        int new_width = original_width;
+        int new_height = original_height;
+
+        // first check if we need to scale width
+        if (original_width > bound_width) {
+            //scale width to fit
+            new_width = bound_width;
+            //scale height to maintain aspect ratio
+            new_height = (new_width * original_height) / original_width;
+        }
+
+        // then check if we need to scale even with the new height
+        if (new_height > bound_height) {
+            //scale height to fit instead
+            new_height = bound_height;
+            //scale width to maintain aspect ratio
+            new_width = (new_height * original_width) / original_height;
+        }
+
+        return new Dimension(new_width, new_height);
     }
 }
